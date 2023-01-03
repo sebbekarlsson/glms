53392de3583be523d7023e4645fc700c24c2c8dc
ianertson <sebbekarlsson97@gmail.com>
2023-01-03 20:57:36 +0100

    only include deps that arent already included

--- cmake/modules/glms_deps.cmake
+++ cmake/modules/glms_deps.cmake
@@ -1,79 +1,48 @@
+macro (glms_get_dep DEP_NAME DEP_URL)
+  set (extra_args ${ARGN})
+  list(LENGTH extra_args extra_count)
+  if (NOT TARGET ${DEP_NAME})
+    FetchContent_Declare(
+      ${DEP_NAME}	
+      GIT_REPOSITORY ${DEP_URL} 
+    )
+    FetchContent_MakeAvailable(${DEP_NAME})
+
+    if (${extra_count} GREATER 0)
+      list(GET extra_args 0 optional_arg)
+      target_compile_options(${DEP_NAME} PUBLIC ${optional_arg})
+    endif ()
+  endif()
+endmacro()
+
 function (glms_deps TARGET_NAME)
-include(FetchContent)
-
-FetchContent_Declare(
-  memo_static
-#  URL /home/ianertson/workspace/memo
-  GIT_REPOSITORY https://github.com/sebbekarlsson/memo.git 
-)
-FetchContent_MakeAvailable(memo_static)
-
-FetchContent_Declare(
-  arena_static
-# URL /home/ianertson/workspace/arena
-  GIT_REPOSITORY https://github.com/sebbekarlsson/arena.git 
-)
-FetchContent_MakeAvailable(arena_static)
-
-FetchContent_Declare(
-  text_static
-#  URL /home/ianertson/workspace/text
-  GIT_REPOSITORY https://github.com/sebbekarlsson/text.git 
-)
-FetchContent_MakeAvailable(text_static)
-
-FetchContent_Declare(
-  hashy_static
-#  URL /home/ianertson/workspace/hashy
-  GIT_REPOSITORY https://github.com/sebbekarlsson/hashy.git 
-)
-FetchContent_MakeAvailable(hashy_static)
+  include(FetchContent)
 
 
-FetchContent_Declare(
-  mif_static
-#  URL /home/ianertson/workspace/mif
-  GIT_REPOSITORY https://github.com/sebbekarlsson/mif.git 
-)
-FetchContent_MakeAvailable(mif_static)
-
-FetchContent_Declare(
-  vec3_static
-#  URL /home/ianertson/workspace/vec3
-  GIT_REPOSITORY https://github.com/sebbekarlsson/vec3.git 
-)
-FetchContent_MakeAvailable(vec3_static)
-
-FetchContent_Declare(
-  spath_static
-#  URL /home/ianertson/workspace/spath
-  GIT_REPOSITORY https://github.com/sebbekarlsson/spath.git 
-)
-FetchContent_MakeAvailable(spath_static)
-target_compile_options(spath_static PUBLIC -fPIC)
-
-FetchContent_Declare(
-  gimg_static
-#  URL /home/ianertson/workspace/gimg
-  GIT_REPOSITORY https://github.com/sebbekarlsson/gimg.git 
-)
-FetchContent_MakeAvailable(gimg_static)
-target_compile_options(gimg_static PUBLIC -fPIC)
+  glms_get_dep(memo_static https://github.com/sebbekarlsson/memo.git)
+  glms_get_dep(arena_static https://github.com/sebbekarlsson/arena.git)
+  glms_get_dep(text_static https://github.com/sebbekarlsson/text.git)
+  glms_get_dep(hashy_static https://github.com/sebbekarlsson/hashy.git)
+  glms_get_dep(mif_static https://github.com/sebbekarlsson/mif.git)
+  glms_get_dep(vec3_static https://github.com/sebbekarlsson/vec3.git)
+  glms_get_dep(spath_static https://github.com/sebbekarlsson/spath.git -fPIC)
+  glms_get_dep(gimg_static https://github.com/sebbekarlsson/gimg.git -fPIC)
 
 
-if (NOT TARGET cglm)
-FetchContent_Declare(
-  cglm
-  GIT_REPOSITORY https://github.com/recp/cglm.git
-  GIT_TAG "v0.8.4"
-  )
-FetchContent_MakeAvailable(cglm)
-endif()
+
+  if (NOT TARGET cglm)
+    FetchContent_Declare(
+      cglm
+      GIT_REPOSITORY https://github.com/recp/cglm.git
+      GIT_TAG "v0.8.4"
+    )
+    FetchContent_MakeAvailable(cglm)
+  endif()
 
 
 
 
-set(GLMS_DEPS m gimg_static memo_static arena_static hashy_static text_static mif_static vec3_static spath_static cglm dl)
+  set(GLMS_DEPS m gimg_static memo_static arena_static hashy_static text_static mif_static vec3_static spath_static cglm dl)
 
-target_link_libraries(${TARGET_NAME} ${GLMS_DEPS})
+  target_link_libraries(${TARGET_NAME} ${GLMS_DEPS})
 endfunction()
